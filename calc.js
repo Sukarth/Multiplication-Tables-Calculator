fa0e3a32e58f94e5f525e599eddcd4de0b4adaf2
Sukarth <sukarthacharya@gmail.com>
2020-09-26 18:17:32 +0300

    first commit

---
+++ calc.js
@@ -0,0 +1,34 @@
+var inputNum = document.getElementById('inputNum');
+var Answer = document.getElementById('Answer')
+var form = document.getElementById('form');
+
+
+
+form.addEventListener('submit', function (event) {
+
+    if (!inputNum.value) {
+        console.error("Please enter values in the field")
+        alert("Please enter values in the field")
+    } else {
+        var inputNum = parseFloat(inputNum.value);
+
+        var storedNum = 0;
+        var counter = 0;
+        counter = 0;
+        while (counter < 10) {
+            event.preventDefault();
+            storedNum = storedNum + inputNum;
+            counter = counter + 1;
+            var h3 = document.createElement("h3");
+            var node = document.createTextNode(storedNum);
+            h3.appendChild(node);
+            var element = document.getElementById("resultField");
+            element.appendChild(h3);
+
+            Answer.innerText = "Answer:"
+
+
+        }
+
+    }
+});
